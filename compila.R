d15f2e752d0b10d29c336b1fad0d551def60865e
pacoalonso <alonsarp@um.es>
2014-10-19 18:53:02 +0200

    Edit some files

--- compila.R
+++ compila.R
@@ -6,3 +6,10 @@ author("pp")
 slidify("index.Rmd")
 browseURL("index.html")
 
+
+git init
+git add * 
+git commit -a -m "Added all of the files"
+git remote add origin http://github.com/pacoalonso/slidify.git
+git pull
+git push origin master
